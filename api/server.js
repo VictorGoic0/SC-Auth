e833290fa4093754822fe44bda835a77dc31bb0f
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-05 11:39:52 -0400

    Added my own helpers, refactored how authRouter and authentication were set up

--- api/server.js
+++ api/server.js
@@ -1,15 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-
-const configureRoutes = require('../config/routes.js');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-
-configureRoutes(server);
 
 module.exports = server;
