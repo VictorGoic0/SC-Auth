e22086c9a93022f53cfe33faf03c25d8e01b345a
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-05 12:34:13 -0400

    Jokes component built out. Paths added to NavLinks

--- dadjokes/src/App.js
+++ dadjokes/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Route } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+import NavBar from "./NavBar";
+import Jokes from "./Jokes";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <NavBar />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/jokes" component={Jokes} />
+      </div>
+    );
   }
 }
 
